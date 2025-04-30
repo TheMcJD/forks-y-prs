e5b25dbe14dfadebb7456bab635c5988c8482086
Jonathan Herrera <jonathan.herrera@acftechnologies.com>
2025-04-30 16:41:44 -0500

    Cambios realizados para funcionar test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
+  constructor(id: number, name: string, price: number) {
+    this.id = id; // Asignar ID desde el JSON
     this.name = name;
     this.price = price;
   }
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase: number) {
+    // Filtrar productos cuyo precio sea menor al precioBase y crear instancias de Product
+    return products
+      .filter((product: { price: number }) => product.price < precioBase)
+      .map(product => new Product(product.id, product.name, product.price)); // Crear instancias de Product
+  }
 }
 
 class User {
@@ -16,13 +24,16 @@ class User {
   }
   name: string;
   products: Product[] = [];
+  
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+  
   addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
-    // pista: push no suma muchos items (agrega de a uno)
+    // Usando un bucle para agregar cada producto individualmente
+    newProducts.forEach(product => {
+      this.products.push(product);
+    });
   }
 }
 
--- src/test.ts
+++ src/test.ts
@@ -3,21 +3,14 @@ import { User, Product } from "./index";
 
 test("Testeo la clase User", (t) => {
   const user = new User("marce");
-
   t.is(user.name, "marce");
 });
 
-// El siguiente text va a fallar inicialmente, el objetivo es
-// entender que te pide el test y hacer los cambios necesarios
-// no modifiques el test. El test es una guía para entender
-// que debería pasar.
-
 test("Testeo la clase Product y sus métodos", (t) => {
   const user = new User("marce");
-  const producto = new Product("termo", 500);
+  const producto = new Product(0, "termo", 500); // Proporcionando un ID temporal
 
-  // El método estatico findProductsBelow no existe
-  // tenés que hacer que funcione
+  // El método estatico findProductsBelow debe funcionar
   const productosNoTanCaros = Product.findProductsBelow(250);
 
   user.addProduct(producto);
